4b8d86caedc636f38ac663da894b6071dce0ede6
Joel Herndon <joel.herndon@gmail.com>
2017-10-19 20:18:35 -0400

    Fixed comma issue in app.R (exercise 1)

--- exercises/exercise1/app.R
+++ exercises/exercise1/app.R
@@ -23,7 +23,7 @@ ui <- fluidPage(
                         label = "Number of bins:",
                         min = 1, 
                         max = 50,
-                        value = 30))
+                        value = 30)),
         # Show a plot of the generated distribution
         mainPanel(
             plotOutput("distPlot") 
